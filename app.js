b566c52f087a54b55a20f4c39edf1c01d4198396
André Santos <andrefs@cpan.org>
2013-12-16 00:06:12 +0000

    Adds logging, static content server and common express settings

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
-
-    app = express();
+	routes  = require('./routes'),
+	path    = require('path'),
+	http    = require('http'),
+    app     = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser(process.env.COOKIE_SECRET || 'RUJtVAafttBDqoxMiEgiiZQnsmC'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.render('home');
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/',      routes.index);
+app.get('/video', routes.video);
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' +
+	app.get('port'));
 });
 
-app.get('/video', function (req,res){
-	var videoid;
-	if(req.query.videoid.match(/v=(\w+)/)) 	videoid = RegExp.$1;
-	else 									videoid = req.query.videoid;
-	res.render('video', {
-		videoid:      videoid,
-		soundtrack:   req.query.soundtrack,
-		playbackrate: req.query.playbackrate,
-		mute:         req.query.mute
-	});
-});
 
-app.listen(3000);
