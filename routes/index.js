6310bbb7e3801635c10d67f093e6fb24048892b1
André Santos <andrefs@cpan.org>
2013-12-19 01:53:40 +0000

    Generates short url with goo.gl

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,41 @@ exports.video = function (req,res){
 	var videoid;
 	if(req.query.videoid.match(/v=(\w+)/)) 	videoid = RegExp.$1;
 	else 									videoid = req.query.videoid;
-	res.render('video', {
-		videoid:      videoid,
-		soundtrack:   req.query.soundtrack,
-		playbackrate: req.query.playbackrate,
-		mute:         req.query.mute
+
+	var full_url  = req.protocol + "://" + req.get('host') + req.url;
+	get_short_url(full_url, function(short_url){
+		console.log(short_url);
+		res.render('video', {
+			short_url    :  short_url,
+			videoid      :  videoid,
+			soundtrack   :  req.query.soundtrack,
+			playbackrate :  req.query.playbackrate,
+			mute         :  req.query.mute
+		});
 	});
+
 };
 
+
+function get_short_url(full_url, callback){
+	var https = require('https');
+	var options = {
+		method  :  'POST',
+		host    :  'www.googleapis.com',
+		path    :  '/urlshortener/v1/url',
+		headers :  {'content-type':'application/json'}
+	};
+	var req = https.request(options, function(response){
+		var str = '';
+		response.on('data', function (chunk) {
+		  str += chunk;
+		});
+		
+		response.on('end', function () {
+			var json = JSON.parse(str);
+		  callback(json.id);
+		});
+	});
+	req.write('{"longUrl": "'+full_url+'"}');
+	req.end();
+};
